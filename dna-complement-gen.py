f84ea184327ff6de7c6eae438fb459e7b040158f
eben-vranken <vranken.eben@outlook.com>
2026-07-08 22:40:21 +0200

    feat: add reverse flag and logic

--- dna-complement-gen.py
+++ dna-complement-gen.py
@@ -6,11 +6,19 @@ def get_args():
 
     parser.add_argument("dna")
 
+    parser.add_argument("-c", "--complement", action="store_true", help="Flag to enable complement generation of the DNA string")
+    parser.add_argument("-r", "--reverse", action="store_true", help="Flag to enable reversing of the DNA string")
+
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = get_args()
 
-    complement = transformer.invert_dna_string(args.dna)
-    
-    print(complement)
+    output = args.dna
+
+    if args.complement:
+        output = transformer.invert_dna_string(output)
+    if args.reverse:
+        output = transformer.reverse_dna_string(output)
+
+    print(output)
--- src/transformer.py
+++ src/transformer.py
@@ -1,5 +1,6 @@
 def invert_dna_string(dna_string):
     output = ""
+    
     for c in dna_string.upper():
         match c:
             case "A":
@@ -12,3 +13,6 @@ def invert_dna_string(dna_string):
                 output += "C"
         
     return output
+
+def reverse_dna_string(dna_string):
+    return dna_string[::-1]
